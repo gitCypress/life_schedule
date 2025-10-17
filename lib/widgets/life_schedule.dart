ab2b0eaa5217d8cf79bf90845e59cf3dd8e91ae6
DesCypress <2730196143@qq.com>
2025-10-17 15:09:56 +0800

    - 初步完成 go_router 导航包的破坏性引入 - 异常功能：Fab Mixin - 异常功能：Nav Mixin   - 标题未配置   - 底栏图标和文字手动配置

--- lib/widgets/life_schedule.dart
+++ lib/widgets/life_schedule.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:life_schedule/config/router.dart';
 
 import '../providers/ui/theme_provider.dart';
 import '../themes/green_theme.dart';
-import 'main_scaffold/main_scaffold.dart';
 
 /// LifeSchedule
 /// 控制标题和应用主题内容
@@ -15,7 +15,7 @@ class LifeSchedule extends ConsumerWidget {
     const appTheme = MaterialGreenTheme();
     final isDarkMode = ref.watch(themeCustomProvider);
 
-    return MaterialApp(
+    return MaterialApp.router(
       /// 供给操作系统使用的元数据
       title: 'LifeSchedule',
       theme: appTheme.light(),
@@ -23,7 +23,7 @@ class LifeSchedule extends ConsumerWidget {
 
       /// 夜间模式
       themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
-      home: const MainScaffold(),
+      routerConfig: router,
     );
   }
 }
